a7b2a07c70427a8cfb3159408d5cf9aadb9988c7
Siddharth <sidp5276@gmail.com>
2026-01-04 20:24:17 +0530

    init commit

---
+++ src/Phase_01/Q_02_Eveodd.java
@@ -0,0 +1,16 @@
+package Phase_01;
+
+    import java.util.Scanner;
+
+    public class Q_02_Eveodd {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int a = sc.nextInt();
+            if (a % 2 == 0) {
+                System.out.println(a + " is even number");
+            } else {
+                System.out.println(a + " is an odd number");
+            }
+            sc.close();
+        }
+    }
