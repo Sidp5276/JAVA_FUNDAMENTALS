0449926bac1a7fc70f0b0ffcf20fae507e0432f3
Siddharth <sidp5276@gmail.com>
2026-01-10 21:41:45 +0530

    Vote eligibility

---
+++ src/Phase_01/Q_15_VoteEligibility.java
@@ -0,0 +1,19 @@
+package Phase_01;
+
+import java.util.Scanner;
+
+public class Q_15_VoteEligibility {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+        scanner.close();
+
+        if (age >= 18) {
+            System.out.println("You are eligible to vote.");
+        } else {
+            System.out.println("You are not eligible to vote.");
+        }
+    }
+}
