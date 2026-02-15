95fb88c54860e978356e3c676afd563e0e93c2e8
Siddharth <sidp5276@gmail.com>
2026-02-15 21:12:55 +0530

    1 to 10

---
+++ src/Phase_02/Q1_1to10.java
@@ -0,0 +1,11 @@
+package Phase_02;
+
+public class Q1_1to10 {
+    public static void main(String[] args){
+
+        for(int i =1; i<=10; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
