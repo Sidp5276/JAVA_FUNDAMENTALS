839c733c985803ea1284a60852cdb66e24e6af09
Siddharth <sidp5276@gmail.com>
2026-02-15 21:34:40 +0530

    1 to 100 even

---
+++ src/Phase_02/Q2_Even.java
@@ -0,0 +1,12 @@
+package Phase_02;
+
+public class Q2_Even {
+public static void main(String[] args) {
+    for (int i = 2; i <= 100; i++) {
+        if (i % 2 == 0) {
+            System.out.println(i);
+        }
+    }
+}
+}
+
