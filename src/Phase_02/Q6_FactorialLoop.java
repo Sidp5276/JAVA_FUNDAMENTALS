2f49fe9fab16635fc357573c52b9add330b5bd55
Siddharth <sidp5276@gmail.com>
2026-02-16 22:30:11 +0530

    factorial

---
+++ src/Phase_02/Q6_FactorialLoop.java
@@ -0,0 +1,17 @@
+package Phase_02;
+import java.util.Scanner;
+
+public class Q6_FactorialLoop{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        long fact = 1;
+
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+
+        System.out.println("Factorial of " + n + " = " + fact);
+    }
+}
