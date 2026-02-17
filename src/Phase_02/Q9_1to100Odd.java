5465e38e82f52de03c92a0e734f6587f62b0bd8e
Siddharth <sidp5276@gmail.com>
2026-02-17 14:20:50 +0530

    1 to 100 odd

---
+++ src/Phase_02/Q9_1to100Odd.java
@@ -0,0 +1,11 @@
+package Phase_02;
+
+public class Q9_1to100Odd {
+    public static void main(String[] args) {
+        for(int i = 1; i<=100; i+=2)
+        {
+            System.out.printf(i + "\t");
+        }
+
+    }
+}
