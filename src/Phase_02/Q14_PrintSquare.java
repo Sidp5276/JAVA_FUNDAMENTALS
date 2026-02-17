75d3bfdb35818a3853227b2392e5a60032330036
Siddharth <sidp5276@gmail.com>
2026-02-17 22:26:15 +0530

    Square

---
+++ src/Phase_02/Q14_PrintSquare.java
@@ -0,0 +1,15 @@
+package Phase_02;
+
+import java.util.Scanner;
+
+public class Q14_PrintSquare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        for (int i =1;i<=n; i++)
+        {
+            System.out.printf(i*i + "\t");
+        }
+    }
+}
