8777b1169b73c9ce9a35b9d8265ff01384edccc4
Siddharth <sidp5276@gmail.com>
2026-02-17 12:59:02 +0530

    1 to 100

---
+++ src/Phase_02/Q7_1to100.java
@@ -0,0 +1,10 @@
+package Phase_02;
+
+public class Q7_1to100 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 100; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
