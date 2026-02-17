fee71690f9d4a01730d5b7f0532b2c5bf3e900e4
Siddharth <sidp5276@gmail.com>
2026-02-17 12:59:16 +0530

    10 to 1

---
+++ src/Phase_02/Q8_10to1.java
@@ -0,0 +1,10 @@
+package Phase_02;
+
+public class Q8_10to1 {
+    public static void main(String[] args) {
+        for(int i=10;i>=1;i--)
+        {
+            System.out.println(i);
+        }
+    }
+}
