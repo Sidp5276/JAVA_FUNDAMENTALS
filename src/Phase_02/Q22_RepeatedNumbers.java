a58c1f81609ca251c9738a19f5e64a5e8b62448c
Siddharth <sidp5276@gmail.com>
2026-02-18 20:13:31 +0530

    Repeated Numbers

---
+++ src/Phase_02/Q22_RepeatedNumbers.java
@@ -0,0 +1,17 @@
+package Phase_02;
+
+import java.util.Scanner;
+
+public class Q22_RepeatedNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        for (int i=1; i <= n; i++) {
+            for(int j=1; j <= i; j++) {
+                System.out.print(i + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
