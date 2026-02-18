d2e4a66149e7804d7cab1b2ffc5bdf0f742427e4
Siddharth <sidp5276@gmail.com>
2026-02-18 20:24:10 +0530

    Increase Sequence

---
+++ src/Phase_02/Q23_IncreaseSeq.java
@@ -0,0 +1,17 @@
+package Phase_02;
+
+import java.util.Scanner;
+
+public class Q23_IncreaseSeq {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        for (int i=1; i <= n; i++) {
+            for(int j=1; j <= i; j++) {
+                System.out.print(j + "\t");
+            }
+            System.out.println();
+    }
+}
+}
