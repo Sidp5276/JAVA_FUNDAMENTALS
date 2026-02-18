4142c521e7026b6c0e670e5d361eabe8318aa141
Siddharth <sidp5276@gmail.com>
2026-02-18 17:50:57 +0530

    Right aligned Triangle star

---
+++ src/Phase_02/Q20_RightAlignedTri.java
@@ -0,0 +1,20 @@
+package Phase_02;
+
+import java.util.Scanner;
+
+public class Q20_RightAlignedTri {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n= sc.nextInt();
+        for (int i =1; i <= n; i++) {
+            for(int j=1; j<= n-i; j++) {
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
