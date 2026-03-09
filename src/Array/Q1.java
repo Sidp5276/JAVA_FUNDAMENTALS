7d8142cd0ead5363769d13ea1ccc332cdac813f6
Siddharth <sidp5276@gmail.com>
2026-03-09 22:32:18 +0530

    Array demo

---
+++ src/Array/Q1.java
@@ -0,0 +1,24 @@
+package Array;
+
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of Array: ");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+
+        System.out.println("Enter " + size + " elements:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Array Elements: ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
