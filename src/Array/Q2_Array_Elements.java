0be7ec00988d56248b8fa4809cfcec759e31c71e
Siddharth <sidp5276@gmail.com>
2026-03-09 23:08:11 +0530

    Arrray elements

---
+++ src/Array/Q2_Array_Elements.java
@@ -0,0 +1,16 @@
+//Ways to print elements
+package Array;
+
+public class Q2_Array_Elements {
+    public static void main(String[] args) {
+        int a[] = new int[3];
+        a[0] = 10;
+        a[1] = 20;
+        a[2] = 30;
+
+        for(int i =0; i<3; i++)
+        {
+            System.out.print(a[i] + " ");
+        }
+    }
+}
