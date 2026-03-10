2dffc0f826a442ef152a435f4a5b5e9d21fbf458
Siddharth <sidp5276@gmail.com>
2026-03-10 21:30:40 +0530

    Array foreach loop

---
+++ src/Array/Q3_Foreach_Loop.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class Q3_Foreach_Loop {
+    public static void main(String[] args) {
+        int a[] = {10,20,30,40,50};
+        for(int b : a) {
+            System.out.print(b + " ");
+    }
+       // for(int i=0;i<5; i++){
+         //   System.out.print(a[i] + " ");
+        //}
+}
+}
