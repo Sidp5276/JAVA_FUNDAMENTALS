63164ab03b98225821d7c9617aca3e4509c6afda
Siddharth <sidp5276@gmail.com>
2026-03-14 16:57:28 +0530

    Rhombus

---
+++ src/Apna_College/Q1_Rhombus_Pattern.java
@@ -0,0 +1,19 @@
+package Apna_College;
+import java.util.*;
+public class Q1_Rhombus_Pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i<=n; i++) {
+            for (int j = 1; j<= n-i; j++) {
+                System.out.print(" ");
+            }
+            for(int k = 1; k<= n; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
