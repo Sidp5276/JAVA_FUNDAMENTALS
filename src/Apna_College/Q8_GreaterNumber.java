96206a0734293f4e51cbd768a5eb49111cc81a4f
Siddharth <sidp5276@gmail.com>
2026-03-15 00:36:42 +0530

    Greater number

---
+++ src/Apna_College/Q8_GreaterNumber.java
@@ -0,0 +1,22 @@
+package Apna_College;
+import java.util.*;
+
+public class Q8_GreaterNumber {
+    public static int Greater(int a, int b) {
+        if(a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.println("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.println("Greater num is: " + Greater(a, b));
+    }
+}
