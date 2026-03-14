ea734f32bd68d6799c8984ae0d0bf4288088f3cf
Siddharth <sidp5276@gmail.com>
2026-03-14 16:33:51 +0530

    Pyramid number

---
+++ src/Example_Program/Q22_PyramidNumbers.java
@@ -0,0 +1,16 @@
+package Example_Program;
+import java.util.*;
+public class Q22_PyramidNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
