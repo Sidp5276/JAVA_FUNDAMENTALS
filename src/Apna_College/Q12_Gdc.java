ef913dbfbdaeee75dd6139171b1dea569a7da898
Siddharth <sidp5276@gmail.com>
2026-03-15 15:18:08 +0530

    GDC

---
+++ src/Apna_College/Q12_Gdc.java
@@ -0,0 +1,21 @@
+package Apna_College;
+// Greatest common divisor
+import java.util.*;
+
+public class Q12_Gdc {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+
+
+        while(n1 != n2) {
+            if(n1>n2) {
+                n1 = n1 - n2;
+            } else {
+                n2 = n2 - n1;
+            }
+        }
+        System.out.println("GCD is : "+ n2);
+    }
+}
