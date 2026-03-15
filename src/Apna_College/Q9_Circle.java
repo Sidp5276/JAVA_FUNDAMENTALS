2691ccdec33bb65d5175e82794545170527bba33
Siddharth <sidp5276@gmail.com>
2026-03-15 15:07:38 +0530

    Circle circumference

---
+++ src/Apna_College/Q9_Circle.java
@@ -0,0 +1,12 @@
+package Apna_College;
+import java.util.*;
+public class Q9_Circle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the radius: ");
+        double radius = sc.nextInt();
+
+        double cir = 2 * 3.14 * radius;
+        System.out.println("Circumference  of circle is: " + cir);
+    }
+}
