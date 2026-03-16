d1e1b8587b63ac1f577f2b5e838167acf397bd3f
Siddharth <sidp5276@gmail.com>
2026-03-16 22:25:02 +0530

    X power N

---
+++ src/Apna_College/Q15_X_Power_n.java
@@ -0,0 +1,18 @@
+package Apna_College;
+import java.util.*;
+
+public class Q15_X_Power_n {
+    public static void main(String args[]) {
+        System.out.println("Enter x : ");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println("Enter n : ");
+        int n = sc.nextInt();
+
+        int result = 1;
+        for(int i=0; i<n; i++) {
+            result = result * x;
+        }
+        System.out.println("x to the power n is : "+ result);
+    }
+}
