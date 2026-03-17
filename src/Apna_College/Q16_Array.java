a792f7203df52daf6ab4214c6d82852a066f15e9
Siddharth <sidp5276@gmail.com>
2026-03-17 20:01:57 +0530

    Array

---
+++ src/Apna_College/Q16_Array.java
@@ -0,0 +1,15 @@
+package Apna_College;
+import java.util.*;
+
+public class Q16_Array {
+    public static void main(String[] args) {
+        int marks[] = new int[3];
+        marks [0] = 95;
+        marks[1] = 98;
+        marks[2] = 90;
+        for(int i =0; i<3; i++)
+        {
+            System.out.println(marks[i]);
+        }
+    }
+}
