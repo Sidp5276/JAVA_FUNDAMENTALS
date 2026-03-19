c066234376138d9b18ff4a3e0b29f7afa0a1da9c
Siddharth <sidp5276@gmail.com>
2026-03-19 08:56:39 +0530

    2d Array

---
+++ src/Apna_College/Q20_2dArray.java
@@ -0,0 +1,24 @@
+package Apna_College;
+import java.util.*;
+public class Q20_2dArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the rows: ");
+        int rows = sc.nextInt();
+        System.out.println("Enter the columns: ");
+        int col = sc.nextInt();
+        int[][] num = new int[rows][col];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < col; j++) {
+                num[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(num[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
