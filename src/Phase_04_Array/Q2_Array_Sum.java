8666bef3f7514dd9d8beb9982ce9740faffe0309
Siddharth <sidp5276@gmail.com>
2026-03-20 21:03:56 +0530

    Array sum

---
+++ src/Phase_04_Array/Q2_Array_Sum.java
@@ -0,0 +1,23 @@
+package Phase_04_Array;
+import java.util.*;
+public class Q2_Array_Sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " integers:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+        System.out.println("Sum of all elements = " + sum);
+    }
+}
+
