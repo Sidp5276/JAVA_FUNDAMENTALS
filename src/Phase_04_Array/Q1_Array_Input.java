21e4a527bc83e0a60a795af7a2dca16c2959b3e3
Siddharth <sidp5276@gmail.com>
2026-03-20 21:00:28 +0530

    Array input

---
+++ src/Phase_04_Array/Q1_Array_Input.java
@@ -0,0 +1,22 @@
+package Phase_04_Array;
+import java.util.*;
+public class Q1_Array_Input {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Enter number of elements: ");
+            int n = sc.nextInt();
+
+            int[] arr = new int[n];
+
+            System.out.println("Enter " + n + " integers:");
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            System.out.println("You entered:");
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
