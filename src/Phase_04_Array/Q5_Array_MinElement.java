78f148fca9ea35b881bd944bef17c1c213854e80
Siddharth <sidp5276@gmail.com>
2026-03-21 21:40:49 +0530

    Array min element

---
+++ src/Phase_04_Array/Q5_Array_MinElement.java
@@ -0,0 +1,26 @@
+package Phase_04_Array;
+import java.util.*;
+public class Q5_Array_MinElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " integers:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int min = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        System.out.println("Minimum element = " + min);
+    }
+}
