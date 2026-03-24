db9a092f5dbb24e2faea3498d4925db0070a4be7
Siddharth <sidp5276@gmail.com>
2026-03-24 20:13:43 +0530

    2D Array

---
+++ src/Phase_04_Array/Q9_Array_2D.java
@@ -0,0 +1,9 @@
+package Phase_04_Array;
+
+public class Q9_Array_2D {
+    public static void main(String[] args) {
+        int[][] myArr = new int[2][3];
+        int[][] arr = {{1, 2, 3}, {4, 5, 6}};
+        System.out.println(arr[0].length);
+    }
+}
