5b85262525a7f5e2d2934993769b3b4436d3ff60
Siddharth <sidp5276@gmail.com>
2026-03-28 22:59:30 +0530

    Array even sum

---
+++ src/Phase_04_Array/Q19_Array_EvenSum.java
@@ -0,0 +1,24 @@
+package Phase_04_Array;
+import java.util.*;
+public class Q19_Array_EvenSum {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Enter the size of array: ");
+            int n = sc.nextInt();
+
+            int[] arr = new int[n];
+
+            System.out.println("Enter " + n + " elements:");
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            int evenSum = 0;
+            for (int i = 0; i < n; i++) {
+                if (arr[i] % 2 == 0) {
+                    evenSum += arr[i];
+                }
+            }
+            System.out.println("Sum of even elements = " + evenSum);
+        }
+    }
