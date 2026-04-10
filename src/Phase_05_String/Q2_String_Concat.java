f6274db5e639988fe57837aa9a09186ae7df03c5
Siddharth <sidp5276@gmail.com>
2026-04-10 22:53:16 +0530

    String concatenation

---
+++ src/Phase_05_String/Q2_String_Concat.java
@@ -0,0 +1,9 @@
+package Phase_05_String;
+
+public class Q2_String_Concat {
+    public static void main(String[] args) {
+        String firstname = "tony";
+        String lastname = "stark";
+        String fullname = firstname + " " + lastname;
+        System.out.println(fullname);
+    } }
