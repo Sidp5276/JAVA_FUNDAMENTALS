88b324beedfc46d26755ab9b7864b1b7bf74143d
Siddharth <sidp5276@gmail.com>
2026-04-10 22:46:27 +0530

    String Example

---
+++ src/Phase_05_String/Q1_String_Example.java
@@ -0,0 +1,10 @@
+package Phase_05_String;
+import java.util.*;
+public class Q1_String_Example {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String: ");
+        String s = sc.nextLine();
+        System.out.println("Your Name is : " + s);
+    }
+}
