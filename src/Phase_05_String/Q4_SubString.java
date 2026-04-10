30bb7095e0fb2d31904e5f4c0bb7453c1628034e
Siddharth <sidp5276@gmail.com>
2026-04-10 23:07:04 +0530

    String substring

---
+++ src/Phase_05_String/Q4_SubString.java
@@ -0,0 +1,9 @@
+package Phase_05_String;
+
+public class Q4_SubString {
+    public static void main(String args[]) {
+        String name = "TonyStark";
+
+        System.out.println(name.substring(0, 4));
+    }
+}
