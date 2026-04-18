e2417d4c1dc116b468d5a18abd9d4edc91845fa4
Siddharth <sidp5276@gmail.com>
2026-04-18 07:35:13 +0530

    String palindrome

---
+++ src/Phase_05_String/Q_21_String_Palindrome.java
@@ -0,0 +1,20 @@
+package Phase_05_String;
+import java.util.Scanner;
+public class Q_21_String_Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string : ");
+        String str = sc.nextLine();
+        System.out.println(isPalindrome(str));
+    }
+    static boolean isPalindrome(String str){
+        for(int i = 0; i<= str.length()/2; i++){
+            char start = str.charAt(i);
+            char end  = str.charAt(str.length()-i-1);
+            if (start != end){
+                return false;
+            }
+        }
+        return true;
+    }
+}
