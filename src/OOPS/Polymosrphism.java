eb4260b160e34b4f4ef9fb6a9bcf555b0080d937
Siddharth <sidp5276@gmail.com>
2026-04-27 19:24:38 +0530

    Polymorphism

---
+++ src/OOPS/Polymosrphism.java
@@ -0,0 +1,27 @@
+package OOPS;
+
+public class Polymosrphism {
+
+    //Compile time polymorphism
+
+    class Student {
+        String name;
+        int age;
+
+        public void displayInfo(String name) {
+            System.out.println(name);
+        }
+
+
+        public void displayInfo(int age) {
+            System.out.println(age);
+        }
+
+
+        public void displayInfo(String name, int age) {
+            System.out.println(name);
+            System.out.println(age);
+        }
+    }
+
+}
