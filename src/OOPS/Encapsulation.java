fbc26f3adbf908b25ba86e38a44ce203ca55b715
Siddharth <sidp5276@gmail.com>
2026-04-28 14:29:59 +0530

    Encapsulation example

---
+++ src/OOPS/Encapsulation.java
@@ -0,0 +1,26 @@
+package OOPS;
+
+public class Encapsulation {
+
+    public class BankAccount {
+
+        private double balance;
+
+        public BankAccount(double initialBalance) {
+            this.balance = initialBalance;
+        }
+
+        public double getBalance() {
+            return balance;
+        }
+
+        public void deposit(double amount) {
+            if (amount > 0) {
+                balance += amount;
+                System.out.println("Amount deposited: " + amount);
+            } else {
+                System.out.println("Invalid amount!");
+            }
+        }
+    }
+}
