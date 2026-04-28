7bc67b5c6102ce4fd4ee30c0d396ed0b2641abe4
Siddharth <sidp5276@gmail.com>
2026-04-28 14:37:01 +0530

    Overloading vs Overriding

---
+++ src/OOPS/Overloading_Vs_Overriding.java
@@ -0,0 +1,34 @@
+package OOPS;
+
+public class Overloading_Vs_Overriding {
+
+        static class Calculator {
+            public int add(int a, int b) {
+                return a + b;
+            }
+            public int add(int a, int b, int c) {
+                return a + b + c;
+            }
+        }
+
+        static class Animal {
+            public void makeSound() {
+                System.out.println("Animal makes a sound");
+            }
+        }
+
+        static class Dog extends Animal {
+            @Override
+            public void makeSound() {
+                System.out.println("Dog barks");
+            }
+        }
+
+        public static void main(String[] args) {
+            Calculator calc = new Calculator();
+            System.out.println("Overloading Result: " + calc.add(10, 20));
+
+            Animal myAnimal = new Dog();
+            myAnimal.makeSound();
+        }
+    }
