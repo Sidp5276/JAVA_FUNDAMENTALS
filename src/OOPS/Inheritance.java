4cbb7150b21978c635de45c97cbdb3bb13246e87
Siddharth <sidp5276@gmail.com>
2026-04-28 15:11:21 +0530

    Inheritance

---
+++ src/OOPS/Inheritance.java
@@ -0,0 +1,44 @@
+package OOPS;
+
+public class Inheritance {
+
+    static class Vehicle {
+        String brand;
+        int year;
+
+        public void start() {
+            System.out.println("Vehicle is starting...");
+        }
+
+        public void stop() {
+            System.out.println("Vehicle is stopping...");
+        }
+    }
+
+    static class Car extends Vehicle {
+        int doors;
+        public void honk() {
+            System.out.println("Car says: Beep Beep!");
+        }
+    }
+
+    static class Bike extends Vehicle {
+        boolean hasCarrier;
+        public void kickStart() {
+            System.out.println("Bike started with a kick.");
+        }
+    }
+
+    public static void main(String[] args) {
+        // Car object
+        Car myCar = new Car();
+        myCar.brand = "Toyota";
+        myCar.start();
+        myCar.honk();
+
+        Bike myBike = new Bike();
+        myBike.brand = "Yamaha";
+        myBike.kickStart();
+        myBike.stop();
+    }
+}
