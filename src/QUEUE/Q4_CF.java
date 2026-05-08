335a503a8b3ddaab05fef5e1d85da4d1531d918a
Siddharth <sidp5276@gmail.com>
2026-05-08 09:27:11 +0530

    Q4 Collection Framework

---
+++ src/QUEUE/Q4_CF.java
@@ -0,0 +1,21 @@
+package QUEUE;
+import java.util.*;
+// Collection Framework
+
+public class Q4_CF {
+    public static void main(String args[]) {
+        //Queue<Integer> q = new LinkedList();
+        Queue<Integer> q = new ArrayDeque();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+
+
+        while(!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
