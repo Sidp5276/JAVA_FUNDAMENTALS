0a6b19a1a0494993b537865bbea8c1168c12c33c
Siddharth <sidp5276@gmail.com>
2026-05-09 13:56:10 +0530

    Q1 Implement

---
+++ src/HASHMAP/Q1_Implement.java
@@ -0,0 +1,47 @@
+package HASHMAP;
+import java.util.*;
+
+public class Q1_Implement {
+    // Added 'public' here
+    public static void main(String[] args) {
+
+        // Creation
+        HashMap<String, Integer> map = new HashMap<>();
+
+        // Insertion
+        map.put("India", 120);
+        map.put("US", 30);
+        map.put("China", 150);
+
+        System.out.println(map);
+
+        map.put("China", 180);
+        System.out.println(map);
+
+        // Searching
+        if(map.containsKey("Indonesia")) {
+            System.out.println("key is present in the map");
+        } else {
+            System.out.println("key is not present in the map");
+        }
+
+        System.out.println(map.get("China"));
+        System.out.println(map.get("Indonesia"));
+
+        // Iteration (1)
+        for(Map.Entry<String, Integer> e : map.entrySet()) {
+            System.out.print(e.getKey() + " : ");
+            System.out.println(e.getValue());
+        }
+
+        // Iteration (2)
+        Set<String> keys = map.keySet();
+        for(String key : keys) {
+            System.out.println(key + " " + map.get(key));
+        }
+
+        // Removing
+        map.remove("China");
+        System.out.println(map);
+    }
+}
